80eb5ccce816ce2380a1078f46f96f20a2ffc83a
Gbenga Opeyemi <opesam42@gmail.com>
2024-08-28 11:30:17 +0100

    connect to external stylesheet

--- form/urls.py
+++ form/urls.py
@@ -18,14 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from contactform import views
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('', views.contact_view, name='index'),
     path('success/<str:first_name>/', views.success_view, name='success'),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
